01f51c127fa04213ba9df54d9f45e2154b859419
ProgramerNawaf <105772524+ProgramerNawaf@users.noreply.github.com>
2023-05-23 18:03:02 +0300

    Add files via upload

---
+++ userfrontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Appbar from './components/Appbar'
+import Users from './components/users';
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+      <Users/>
+    </div>
+  );
+}
+
+export default App;
